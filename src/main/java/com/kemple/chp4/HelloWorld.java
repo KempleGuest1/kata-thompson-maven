59e18e6f5cd274ba4426a0b2a99878d359d47575
Craig W. Kemple <ckemple@yahoo.com>
2021-07-11 10:02:56 -0500

    Create base project for future branching.

--- src/main/java/com/kemple/chp4/HelloWorld.java
+++ src/main/java/com/kemple/chp4/HelloWorld.java
@@ -1,8 +1,8 @@
-import org.apache.commons.lang3.StringUtils;
+package com.kemple.chp4;
+
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
